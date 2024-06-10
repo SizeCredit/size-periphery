e701b760b3f4573275d66d402c1a1f2a43b9a80a
Imtiyaz <imtiyazmiah@gmail.com>
2024-06-10 05:31:06 +0100

    update size credit library update scripts to match new dir structure

--- src/DexSwap.sol
+++ src/DexSwap.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.23;
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import {Errors} from "@size/src/core/libraries/Errors.sol";
+import {Errors} from "@size/src/libraries/Errors.sol";
 import {I1InchAggregator} from "src/interfaces/dex/I1InchAggregator.sol";
 import {IUniswapV2Router02} from "src/interfaces/dex/IUniswapV2Router02.sol";
 import {IUnoswapRouter} from "src/interfaces/dex/IUnoswapRouter.sol";

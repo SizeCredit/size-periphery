95a84b07b2e009718cc080f59ce114a7de311a50
Imtiyaz <imtiyazmiah@gmail.com>
2024-06-10 04:39:02 +0100

    log userview

--- script/DepositWETH.s.sol
+++ script/DepositWETH.s.sol
@@ -5,6 +5,7 @@ import "forge-std/Script.sol";
 
 import "@size/src/core/Size.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {UserView} from "@size/src/core/SizeViewData.sol";
 
 interface IWETH {
     function deposit(uint amt) external payable;
@@ -20,6 +21,7 @@ contract DepositWETHScript is Script {
         address lender = vm.envAddress("LENDER");
         address borrower = vm.envAddress("BORROWER");
 
+        Size size = Size(payable(sizeContractAddress));
         IERC20 weth = IERC20(wethAddress);
         uint256 amount = 0.01e18;  // 0.01 WETH
 
@@ -33,18 +35,28 @@ contract DepositWETHScript is Script {
         IWETH weth_deposit = IWETH(wethAddress);
         weth_deposit.deposit(amount);
 
-        Size size = Size(payable(sizeContractAddress));
         DepositParams memory params = DepositParams({token: wethAddress, amount: amount, to: borrower});
         vm.startBroadcast(deployerPrivateKey);
 
         weth.approve(sizeContractAddress, amount);
+        console.log("");
         console.log("WETH Balance of Borrower after approve:", weth.balanceOf(borrower));
         console.log("WETH Allowance after approve:", weth.allowance(borrower, sizeContractAddress));
 
         
         size.deposit(params);
+        console.log("");
         console.log("WETH Balance of Borrower after deposit:", weth.balanceOf(borrower));
         console.log("WETH Allowance after deposit:", weth.allowance(borrower, sizeContractAddress));
+        // Fetch and log user information
+        UserView memory userView = size.getUserView(borrower);
+
+        console.log("");
+        console.log("User:", userView.account);
+        console.log("Collateral Token Balance:", userView.collateralTokenBalance);
+        console.log("Borrow A Token Balance:", userView.borrowATokenBalance);
+        console.log("Debt Balance:", userView.debtBalance);
+
         vm.stopBroadcast();
     }
 }

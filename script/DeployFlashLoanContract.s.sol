1a17c9d31a14a8d51279f1e9689f59776d378e28
imtiyaz <imtiyazmiah@gmail.com>
2024-11-21 19:30:06 +0000

    update with base mainnet addresses

--- script/DeployFlashLoanContract.s.sol
+++ script/DeployFlashLoanContract.s.sol
@@ -27,8 +27,8 @@ contract DeployFlashLoanLiquidator is Script {
         // address size = 0xC2a429681CAd7C1ce36442fbf7A4a68B11eFF940;
         address aggregator1inch = 0x425141165d3DE9FEC831896C016617a52363b687;
         address unoswapRouter = 0x425141165d3DE9FEC831896C016617a52363b687;
-        address uniswapRouter = 0x4752ba5DBc23f44D87826276BF6Fd6b1C372aD24;
-        address uniswapV3Router = 0x4752ba5DBc23f44D87826276BF6Fd6b1C372aD24;  // TODO base address
+        address uniswapRouter = 0x2626664c2603336E57B271c5C0b26F421741e481;
+        address uniswapV3Router = 0x2626664c2603336E57B271c5C0b26F421741e481;
         // address collateralToken = 0x4200000000000000000000000000000000000006;
         // address borrowToken = 0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913;
 

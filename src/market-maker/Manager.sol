94c3e0c724de0c497ee4ef4d10adc76add88a7da
Antonio Guilherme Ferreira Viggiano <agfviggiano@gmail.com>
2024-11-28 13:24:20 -0300

    Simplify contract

--- src/market-maker/Manager.sol
+++ src/market-maker/Manager.sol
@@ -9,8 +9,11 @@ import {BuyCreditLimitParams, SellCreditLimitParams} from "@size/src/interfaces/
 import {Address} from "@openzeppelin/contracts/utils/Address.sol";
 import {WithdrawParams} from "@size/src/interfaces/ISize.sol";
 import {DataView} from "@size/src/SizeViewData.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 contract Manager is UUPSUpgradeable, Ownable2StepUpgradeable {
+    using SafeERC20 for IERC20;
+
     ISizeFactory public sizeFactory;
     address public bot;
 
@@ -18,7 +21,6 @@ contract Manager is UUPSUpgradeable, Ownable2StepUpgradeable {
     event FactorySet(ISizeFactory indexed oldFactory, ISizeFactory indexed newFactory);
 
     error NullAddress();
-    error ArrayLengthsMismatch();
     error OnlyBot();
 
     // @custom:oz-upgrades-unsafe-allow constructor
@@ -52,70 +54,13 @@ contract Manager is UUPSUpgradeable, Ownable2StepUpgradeable {
         _setFactory(_sizeFactory);
     }
 
-    function _setBot(address _bot) internal {
-        emit BotSet(bot, _bot);
-        bot = _bot;
+    function deposit(ISize size, DepositParams memory params) external onlyOwner {
+        params.token.safeTransferFrom(msg.sender, address(this), params.amount);
+        size.deposit(params);
     }
 
-    function _setFactory(ISizeFactory _sizeFactory) internal {
-        emit FactorySet(sizeFactory, _sizeFactory);
-        sizeFactory = _sizeFactory;
-    }
-
-    function proxy(address target, bytes memory data) public onlyOwner returns (bytes memory returnData) {
-        if (target == address(0)) {
-            revert NullAddress();
-        }
-        bool success;
-        (success, returnData) = address(target).call(data);
-        Address.verifyCallResult(success, returnData);
-    }
-
-    function proxy(address target, bytes memory data, uint256 value)
-        public
-        onlyOwner
-        returns (bytes memory returnData)
-    {
-        if (target == address(0)) {
-            revert NullAddress();
-        }
-        bool success;
-        (success, returnData) = address(target).call{value: value}(data);
-        Address.verifyCallResult(success, returnData);
-    }
-
-    function proxy(address[] memory targets, bytes[] memory datas)
-        public
-        onlyOwner
-        returns (bytes[] memory returnDatas)
-    {
-        if (targets.length != datas.length) {
-            revert ArrayLengthsMismatch();
-        }
-
-        returnDatas = new bytes[](datas.length);
-        bool success;
-        for (uint256 i = 0; i < targets.length; i++) {
-            if (targets[i] == address(0)) {
-                revert NullAddress();
-            }
-            // slither-disable-next-line calls-loop
-            (success, returnDatas[i]) = address(targets[i]).call(datas[i]);
-            Address.verifyCallResult(success, returnDatas[i]);
-        }
-    }
-
-    function emergencyWithdraw() external onlyOwner {
-        ISize[] memory sizes = sizeFactory.getMarkets();
-        for (uint256 i = 0; i < sizes.length; i++) {
-            DataView memory data = sizes[i].data();
-            sizes[i].withdraw(
-                WithdrawParams({to: owner(), token: address(data.underlyingCollateralToken), amount: type(uint256).max})
-            );
-            sizes[i].withdraw(
-                WithdrawParams({to: owner(), token: address(data.underlyingBorrowToken), amount: type(uint256).max})
-            );
-        }
+    function withdraw(ISize size, WithdrawParams memory params) external onlyOwner {
+        size.withdraw(params);
     }
 
     function buyCreditLimit(ISize size, BuyCreditLimitParams memory params) external onlyBot {
@@ -124,6 +69,16 @@ contract Manager is UUPSUpgradeable, Ownable2StepUpgradeable {
 
     function sellCreditLimit(ISize size, SellCreditLimitParams memory params) external onlyBot {
         size.sellCreditLimit(params);
+    }
+
+    function _setBot(address _bot) internal {
+        emit BotSet(bot, _bot);
+        bot = _bot;
+    }
+
+    function _setFactory(ISizeFactory _sizeFactory) internal {
+        emit FactorySet(sizeFactory, _sizeFactory);
+        sizeFactory = _sizeFactory;
     }
 
     receive() external payable {}

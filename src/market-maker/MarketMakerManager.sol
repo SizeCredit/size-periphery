57361f703473abc6102754e7eccf06f7b545e248
Antonio Guilherme Ferreira Viggiano <agfviggiano@gmail.com>
2024-11-28 15:25:08 -0300

    Rename market maker contract

--- src/market-maker/MarketMakerManager.sol
+++ src/market-maker/MarketMakerManager.sol
@@ -4,23 +4,18 @@ pragma solidity 0.8.23;
 import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import {Ownable2StepUpgradeable} from "@openzeppelin/contracts-upgradeable/access/Ownable2StepUpgradeable.sol";
 import {ISize} from "@size/src/interfaces/ISize.sol";
-import {ISizeFactory} from "@size/src/v1.5/interfaces/ISizeFactory.sol";
 import {BuyCreditLimitParams, SellCreditLimitParams} from "@size/src/interfaces/ISize.sol";
-import {Address} from "@openzeppelin/contracts/utils/Address.sol";
-import {WithdrawParams} from "@size/src/interfaces/ISize.sol";
-import {DataView} from "@size/src/SizeViewData.sol";
+import {DepositParams, WithdrawParams} from "@size/src/interfaces/ISize.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {IERC20Metadata} from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 
-contract Manager is UUPSUpgradeable, Ownable2StepUpgradeable {
-    using SafeERC20 for IERC20;
+contract MarketMakerManager is UUPSUpgradeable, Ownable2StepUpgradeable {
+    using SafeERC20 for IERC20Metadata;
 
-    ISizeFactory public sizeFactory;
     address public bot;
 
     event BotSet(address indexed oldBot, address indexed newBot);
-    event FactorySet(ISizeFactory indexed oldFactory, ISizeFactory indexed newFactory);
 
-    error NullAddress();
     error OnlyBot();
 
     // @custom:oz-upgrades-unsafe-allow constructor
@@ -28,13 +23,12 @@ contract Manager is UUPSUpgradeable, Ownable2StepUpgradeable {
         _disableInitializers();
     }
 
-    function initialize(address _owner, address _bot, ISizeFactory _sizeFactory) public initializer {
+    function initialize(address _owner, address _bot) public initializer {
         __Ownable2Step_init();
         __Ownable_init(_owner);
         __UUPSUpgradeable_init();
 
         _setBot(_bot);
-        _setFactory(_sizeFactory);
     }
 
     function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
@@ -50,12 +44,9 @@ contract Manager is UUPSUpgradeable, Ownable2StepUpgradeable {
         _setBot(_bot);
     }
 
-    function setFactory(ISizeFactory _sizeFactory) external onlyOwner {
-        _setFactory(_sizeFactory);
-    }
-
     function deposit(ISize size, DepositParams memory params) external onlyOwner {
-        params.token.safeTransferFrom(msg.sender, address(this), params.amount);
+        IERC20Metadata(params.token).safeTransferFrom(msg.sender, address(this), params.amount);
+        IERC20Metadata(params.token).forceApprove(address(size), params.amount);
         size.deposit(params);
     }
 
@@ -74,11 +65,6 @@ contract Manager is UUPSUpgradeable, Ownable2StepUpgradeable {
     function _setBot(address _bot) internal {
         emit BotSet(bot, _bot);
         bot = _bot;
-    }
-
-    function _setFactory(ISizeFactory _sizeFactory) internal {
-        emit FactorySet(sizeFactory, _sizeFactory);
-        sizeFactory = _sizeFactory;
     }
 
     receive() external payable {}

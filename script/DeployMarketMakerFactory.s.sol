dd6cec381902b5681b548b6abf8453df592aa761
Antonio Guilherme Ferreira Viggiano <agfviggiano@gmail.com>
2024-12-17 13:40:40 -0300

    Deploy MarketMakerManagerFactory

--- script/DeployMarketMakerFactory.s.sol
+++ script/DeployMarketMakerFactory.s.sol
@@ -1,6 +1,5 @@
 // SPDX-License-Identifier: MIT
-
-pragma solidity ^0.8.0;
+pragma solidity 0.8.23;
 
 import {Script, console} from "forge-std/Script.sol";
 import {MarketMakerManagerFactory} from "src/MarketMakerManagerFactory.sol";

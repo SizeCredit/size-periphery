0a6c85e11825175bf37f51faea679377747fbc95
Antonio Guilherme Ferreira Viggiano <agfviggiano@gmail.com>
2024-12-19 11:07:29 -0300

    Deploy new impl

--- script/DeployMarketMakerManagerFactory.s.sol
+++ script/DeployMarketMakerManagerFactory.s.sol
@@ -5,7 +5,7 @@ import {Script, console} from "forge-std/Script.sol";
 import {MarketMakerManagerFactory} from "src/MarketMakerManagerFactory.sol";
 import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 
-contract DeployMarketMakerFactory is Script {
+contract DeployMarketMakerManagerFactory is Script {
     function run() external {
         vm.startBroadcast();
 
---
+++ script/DeployMarketMakerManagerImplementation.s.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.23;
+
+import {Script, console} from "forge-std/Script.sol";
+import {MarketMakerManager} from "src/MarketMakerManager.sol";
+
+contract DeployMarketMakerManagerImplementation is Script {
+    function run() external {
+        vm.startBroadcast();
+
+        MarketMakerManager implementation = new MarketMakerManager();
+
+        console.log("Deployed MarketMakerManager at:", address(implementation));
+
+        vm.stopBroadcast();
+    }
+}

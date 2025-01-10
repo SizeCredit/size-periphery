30be33df5ef330edd210aec4e1f9f270f66ae183
imtiyaz <imtiyazmiah@gmail.com>
2025-01-10 11:45:44 +0000

    log outputs

--- script/DeployMarketMakerManagerFactory.s.sol
+++ script/DeployMarketMakerManagerFactory.s.sol
@@ -9,8 +9,10 @@ contract DeployMarketMakerManagerFactory is Script {
     function run() external {
         vm.startBroadcast();
 
-        address governance = vm.envAddress("GOVERNANCE");
-        address bot = vm.envAddress("BOT");
+        // address governance = vm.envAddress("GOVERNANCE");
+        // address bot = vm.envAddress("BOT");
+        address governance = 0x462B545e8BBb6f9E5860928748Bfe9eCC712c3a7;
+        address bot = 0xDe5C38699a7057a33524F96e62Bb1987C2568816;
 
         MarketMakerManagerFactory factory = MarketMakerManagerFactory(
             address(
@@ -22,5 +24,24 @@ contract DeployMarketMakerManagerFactory is Script {
         );
 
         console.log("Deployed MarketMakerManagerFactory at:", address(factory));
+        exportDeploymentDetails(factory, governance, bot);
+    }
+
+    function exportDeploymentDetails(MarketMakerManagerFactory factory, address governance, address bot) internal {
+        string memory root = vm.projectRoot();
+        string memory path = string.concat(root, "/deployments/market_maker/");
+        string memory chainIdStr = vm.toString(block.chainid);
+        path = string.concat(path, string.concat(chainIdStr, ".json"));
+
+        string memory finalObject;
+        string memory deploymentsObject;
+
+        deploymentsObject = vm.serializeAddress(".deployments", "MarketMakerManagerFactory", address(factory));
+        deploymentsObject = vm.serializeAddress(".deployments", "governance", governance);
+        deploymentsObject = vm.serializeAddress(".deployments", "bot", bot);
+
+        finalObject = vm.serializeString(".deployments", "deployments", deploymentsObject);
+        vm.writeJson(finalObject, path);
     }
 }
+

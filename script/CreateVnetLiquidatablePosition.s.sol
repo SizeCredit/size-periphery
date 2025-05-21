78ab0ffe9f8d2f890efe4b4fc3c817f2135b3073
Antonio Guilherme Ferreira Viggiano <agfviggiano@gmail.com>
2025-05-21 14:20:42 -0300

    Update CreateVnetLiquidatablePosition script

--- script/CreateVnetLiquidatablePosition.s.sol
+++ script/CreateVnetLiquidatablePosition.s.sol
@@ -16,6 +16,7 @@ import {DepositParams} from "@src/market/libraries/actions/Deposit.sol";
 import {BuyCreditLimitParams} from "@src/market/libraries/actions/BuyCreditLimit.sol";
 import {SellCreditMarketParams} from "@src/market/libraries/actions/SellCreditMarket.sol";
 import {UpdateConfigParams} from "@src/market/libraries/actions/UpdateConfig.sol";
+import {ICreateX} from "./interfaces/ICreateX.sol";
 
 contract CreateVnetLiquidatablePosition is Script, Addresses {
     using Tenderly for *;
@@ -28,41 +29,55 @@ contract CreateVnetLiquidatablePosition is Script, Addresses {
 
     address borrower = address(0x1111);
     address lender = address(0x2222);
-    address PRICE_FEED_MOCK_ADDRESS = 0xF4a21Ac7e51d17A0e1C8B59f7a98bb7A97806f14;
+
+    ICreateX CreateX = ICreateX(0xba5Ed099633D3B313e4D5F7bdc1305d3c28ba5Ed);
 
     constructor() {
-        vm.createSelectFork("vnet");
-
         vnetId = vm.envString("VNET_ID");
         tenderly.initialize(
             vm.envString("TENDERLY_ACCOUNT_NAME"),
             vm.envString("TENDERLY_PROJECT_NAME"),
             vm.envString("TENDERLY_ACCESS_KEY")
         );
+        Tenderly.VirtualTestnet memory vnet = tenderly.getVirtualTestnetById(vnetId);
+        vm.createSelectFork(vnet.getAdminRpcUrl());
+
         sizeFactory = ISizeFactory(addresses[block.chainid][CONTRACT.SIZE_FACTORY]);
         owner = addresses[block.chainid][CONTRACT.SIZE_GOVERNANCE];
-        size = sizeFactory.getMarket(1);
     }
 
-    function _tenderlyDeposit(address user, address token, uint256 amount) internal {
+    function _tenderlyDeployPriceFeedMock(address _owner) internal returns (address) {
+        bytes memory initCode = abi.encodePacked(type(PriceFeedMock).creationCode, abi.encode(_owner));
+        // https://github.com/pcaversaccio/createx/blob/f83e2a40dbc2db26804e9c8540cd7f1bfd7b323c/src/CreateX.sol#L874
+        bytes32 salt = bytes32(abi.encodePacked(_owner, hex"00", hex"1212121212121212121212"));
+        bytes32 initCodeHash = keccak256(initCode);
+        bytes32 guardedSalt = keccak256(abi.encodePacked(bytes32(uint256(uint160(_owner))), salt));
+        tenderly.sendTransaction(
+            vnetId, _owner, address(CreateX), abi.encodeCall(ICreateX.deployCreate2, (salt, initCode))
+        );
+        address computedAddress = CreateX.computeCreate2Address(guardedSalt, initCodeHash);
+        return computedAddress;
+    }
+
+    function _tenderlyDeposit(address _user, address _token, uint256 _amount) internal {
         Tenderly.VirtualTestnet memory vnet = tenderly.getVirtualTestnetById(vnetId);
-        tenderly.setErc20Balance(vnet, token, user, amount);
-        tenderly.sendTransaction(vnetId, user, token, abi.encodeCall(IERC20.approve, (address(size), amount)));
+        tenderly.setErc20Balance(vnet, _token, _user, _amount);
+        tenderly.sendTransaction(vnetId, _user, _token, abi.encodeCall(IERC20.approve, (address(size), _amount)));
         tenderly.sendTransaction(
             vnetId,
-            user,
+            _user,
             address(size),
-            abi.encodeCall(ISize.deposit, (DepositParams({token: token, amount: amount, to: user})))
+            abi.encodeCall(ISize.deposit, (DepositParams({token: _token, amount: _amount, to: _user})))
         );
     }
 
-    function _tenderlyBuyCreditLimit(address user, uint256 maxDueDate, YieldCurve memory curve) internal {
+    function _tenderlyBuyCreditLimit(address _user, uint256 _maxDueDate, YieldCurve memory _curve) internal {
         tenderly.sendTransaction(
             vnetId,
-            user,
+            _user,
             address(size),
             abi.encodeCall(
-                ISize.buyCreditLimit, (BuyCreditLimitParams({maxDueDate: maxDueDate, curveRelativeTime: curve}))
+                ISize.buyCreditLimit, (BuyCreditLimitParams({maxDueDate: _maxDueDate, curveRelativeTime: _curve}))
             )
         );
     }
@@ -105,41 +120,40 @@ contract CreateVnetLiquidatablePosition is Script, Addresses {
         );
     }
 
-    function _tenderlyUpdatePrice(address _owner, PriceFeedMock _priceFeedMock, uint256 _price) internal {
-        tenderly.sendTransaction(
-            vnetId, _owner, address(_priceFeedMock), abi.encodeCall(PriceFeedMock.setPrice, (_price))
-        );
+    function _tenderlyUpdatePrice(address _owner, address _priceFeedMock, uint256 _price) internal {
+        tenderly.sendTransaction(vnetId, _owner, _priceFeedMock, abi.encodeCall(PriceFeedMock.setPrice, (_price)));
     }
 
     function run() external {
-        IERC20Metadata underlyingCollateralToken = IERC20Metadata(size.data().underlyingCollateralToken);
-        IERC20Metadata underlyingBorrowToken = IERC20Metadata(size.data().underlyingBorrowToken);
+        address priceFeedMock = _tenderlyDeployPriceFeedMock(owner);
 
-        uint256 collateralAmount = 1_200e18;
-        uint256 borrowAmount = 1_000e6;
+        for (uint256 i = 0; i < sizeFactory.getMarkets().length; i++) {
+            size = sizeFactory.getMarket(i);
+            IERC20Metadata underlyingCollateralToken = IERC20Metadata(size.data().underlyingCollateralToken);
+            IERC20Metadata underlyingBorrowToken = IERC20Metadata(size.data().underlyingBorrowToken);
 
-        _tenderlyDeposit(borrower, address(underlyingCollateralToken), collateralAmount);
-        _tenderlyDeposit(lender, address(underlyingBorrowToken), 2 * borrowAmount);
-        YieldCurve memory curve;
-        curve.tenors = new uint256[](2);
-        curve.aprs = new int256[](2);
-        curve.marketRateMultipliers = new uint256[](2);
+            uint256 collateralAmount = 1_200e18;
+            uint256 borrowAmount = 1_000e6;
 
-        curve.aprs[0] = 0.03e18;
-        curve.tenors[0] = 30 days;
-        curve.aprs[1] = 0.04e18;
-        curve.tenors[1] = 60 days;
+            _tenderlyDeposit(borrower, address(underlyingCollateralToken), collateralAmount);
+            _tenderlyDeposit(lender, address(underlyingBorrowToken), 2 * borrowAmount);
+            YieldCurve memory curve;
+            curve.tenors = new uint256[](2);
+            curve.aprs = new int256[](2);
+            curve.marketRateMultipliers = new uint256[](2);
 
-        _tenderlyBuyCreditLimit(lender, block.timestamp + 90 days, curve);
-        _tenderlySellCreditMarket(borrower, lender, RESERVED_ID, borrowAmount, 45 days, false);
-        (uint256 debtPositionsCount,) = size.getPositionsCount();
-        uint256 debtPositionId = DEBT_POSITION_ID_START + debtPositionsCount - 1;
+            curve.aprs[0] = 0.03e18;
+            curve.tenors[0] = 30 days;
+            curve.aprs[1] = 0.04e18;
+            curve.tenors[1] = 60 days;
 
-        PriceFeedMock priceFeedMock = PriceFeedMock(PRICE_FEED_MOCK_ADDRESS);
+            _tenderlyUpdateConfig(owner, "priceFeed", uint256(uint160(address(priceFeedMock))));
+            _tenderlyUpdatePrice(owner, priceFeedMock, 1.0e18);
 
-        _tenderlyUpdateConfig(owner, "priceFeed", uint256(uint160(address(priceFeedMock))));
-        _tenderlyUpdatePrice(owner, priceFeedMock, 0.9e18);
+            _tenderlyBuyCreditLimit(lender, block.timestamp + 90 days, curve);
+            _tenderlySellCreditMarket(borrower, lender, RESERVED_ID, borrowAmount, 45 days, false);
 
-        console.log("debtPositionId", debtPositionId);
+            _tenderlyUpdatePrice(owner, priceFeedMock, 0.9e18);
+        }
     }
 }

df5184ccb71c55467029ab32e46fd04e0e9a2401
imtiyaz <imtiyazmiah@gmail.com>
2025-05-23 16:29:28 +0100

    repay params enum defines operation path unambiguously

--- src/authorization/AutoRollover.sol
+++ src/authorization/AutoRollover.sol
@@ -6,6 +6,7 @@ import {DebtPosition, RESERVED_ID} from "@size/src/market/libraries/LoanLibrary.
 import {DataView} from "@size/src/market/SizeViewData.sol";
 import {PeripheryErrors} from "src/libraries/PeripheryErrors.sol";
 import {RepayParams} from "@size/src/market/libraries/actions/Repay.sol";
+import {WithdrawParams} from "@size/src/market/libraries/actions/Withdraw.sol";
 import {
     SellCreditMarketParams,
     SellCreditMarketOnBehalfOfParams
@@ -17,15 +18,22 @@ import {IERC20Metadata} from "@openzeppelin/contracts/token/ERC20/extensions/IER
 import {Ownable2Step} from "@openzeppelin/contracts/access/Ownable2Step.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {Errors} from "@size/src/market/libraries/Errors.sol";
+import {DexSwap, SwapParams} from "src/liquidator/DexSwap.sol";
 
-contract AutoRollover is Ownable2Step, FlashLoanReceiverBase {
+contract AutoRollover is Ownable2Step, FlashLoanReceiverBase, DexSwap {
     using SafeERC20 for IERC20Metadata;
+
+    enum OperationType {
+        Rollover,
+        Repay
+    }
 
     uint256 public constant EARLY_REPAYMENT_BUFFER = 1 hours;
     uint256 public constant MIN_TENOR = 1 hours;
     uint256 public constant MAX_TENOR = 7 days;
 
     struct OperationParams {
+        OperationType operationType;
         ISize market;
         uint256 debtPositionId;
         address onBehalfOf;
@@ -35,9 +43,26 @@ contract AutoRollover is Ownable2Step, FlashLoanReceiverBase {
         uint256 deadline;
     }
 
-    constructor(address _owner, IPoolAddressesProvider _addressProvider)
+    struct RepayOperationParams {
+        OperationType operationType;
+        ISize market;
+        uint256 debtPositionId;
+        address onBehalfOf;
+        uint256 collateralWithdrawAmount;
+        SwapParams swapParams;
+    }
+
+    constructor(
+        address _owner,
+        IPoolAddressesProvider _addressProvider,
+        address _oneInchAggregator,
+        address _unoswapRouter,
+        address _uniswapV2Router,
+        address _uniswapV3Router
+    )
         Ownable(_owner)
         FlashLoanReceiverBase(_addressProvider)
+        DexSwap(_oneInchAggregator, _unoswapRouter, _uniswapV2Router, _uniswapV3Router)
     {}
 
     function rollover(
@@ -61,6 +86,7 @@ contract AutoRollover is Ownable2Step, FlashLoanReceiverBase {
         }
 
         OperationParams memory operationParams = OperationParams({
+            operationType: OperationType.Rollover,
             market: market,
             debtPositionId: debtPositionId,
             onBehalfOf: onBehalfOf,
@@ -68,6 +94,41 @@ contract AutoRollover is Ownable2Step, FlashLoanReceiverBase {
             tenor: tenor,
             maxAPR: maxAPR,
             deadline: deadline
+        });
+
+        bytes memory params = abi.encode(operationParams);
+
+        address[] memory assets = new address[](1);
+        assets[0] = address(data.underlyingBorrowToken);
+        uint256[] memory amounts = new uint256[](1);
+        amounts[0] = debtPosition.futureValue;
+        uint256[] memory modes = new uint256[](1);
+        modes[0] = 0;
+
+        POOL.flashLoan(address(this), assets, amounts, modes, address(this), params, 0);
+    }
+
+    function repay(
+        ISize market,
+        uint256 debtPositionId,
+        address onBehalfOf,
+        uint256 collateralWithdrawAmount,
+        SwapParams memory swapParams
+    ) external onlyOwner {
+        DebtPosition memory debtPosition = market.getDebtPosition(debtPositionId);
+        DataView memory data = market.data();
+
+        if (debtPosition.dueDate > block.timestamp + EARLY_REPAYMENT_BUFFER) {
+            revert PeripheryErrors.AUTO_REPAY_TOO_EARLY(debtPosition.dueDate, block.timestamp);
+        }
+
+        RepayOperationParams memory operationParams = RepayOperationParams({
+            operationType: OperationType.Repay,
+            market: market,
+            debtPositionId: debtPositionId,
+            onBehalfOf: onBehalfOf,
+            collateralWithdrawAmount: collateralWithdrawAmount,
+            swapParams: swapParams
         });
 
         bytes memory params = abi.encode(operationParams);
@@ -96,31 +157,62 @@ contract AutoRollover is Ownable2Step, FlashLoanReceiverBase {
             revert PeripheryErrors.NOT_INITIATOR();
         }
 
-        OperationParams memory operationParams = abi.decode(params, (OperationParams));
+        // Decode the operation type first
+        OperationType operationType = abi.decode(params, (OperationType));
 
-        uint256 newFutureValue = amounts[0] + premiums[0];
+        if (operationType == OperationType.Rollover) {
+            OperationParams memory rolloverParams = abi.decode(params, (OperationParams));
+            uint256 newFutureValue = amounts[0] + premiums[0];
 
-        operationParams.market.sellCreditMarketOnBehalfOf(
-            SellCreditMarketOnBehalfOfParams({
-                params: SellCreditMarketParams({
-                    lender: operationParams.lender,
-                    creditPositionId: RESERVED_ID,
-                    amount: newFutureValue,
-                    tenor: operationParams.tenor,
-                    maxAPR: operationParams.maxAPR,
-                    deadline: operationParams.deadline,
-                    exactAmountIn: false
-                }),
-                onBehalfOf: operationParams.onBehalfOf,
-                recipient: address(this)
-            })
-        );
+            rolloverParams.market.sellCreditMarketOnBehalfOf(
+                SellCreditMarketOnBehalfOfParams({
+                    params: SellCreditMarketParams({
+                        lender: rolloverParams.lender,
+                        creditPositionId: RESERVED_ID,
+                        amount: newFutureValue,
+                        tenor: rolloverParams.tenor,
+                        maxAPR: rolloverParams.maxAPR,
+                        deadline: rolloverParams.deadline,
+                        exactAmountIn: false
+                    }),
+                    onBehalfOf: rolloverParams.onBehalfOf,
+                    recipient: address(this)
+                })
+            );
 
-        operationParams.market.repay(
-            RepayParams({debtPositionId: operationParams.debtPositionId, borrower: operationParams.onBehalfOf})
-        );
+            rolloverParams.market.repay(
+                RepayParams({debtPositionId: rolloverParams.debtPositionId, borrower: rolloverParams.onBehalfOf})
+            );
 
-        IERC20Metadata(assets[0]).forceApprove(address(POOL), newFutureValue);
+            IERC20Metadata(assets[0]).forceApprove(address(POOL), newFutureValue);
+        } else {
+            RepayOperationParams memory repayParams = abi.decode(params, (RepayOperationParams));
+            uint256 totalDebt = amounts[0] + premiums[0];
+
+            // Repay the debt position
+            repayParams.market.repay(
+                RepayParams({debtPositionId: repayParams.debtPositionId, borrower: repayParams.onBehalfOf})
+            );
+
+            // Withdraw collateral
+            repayParams.market.withdraw(
+                WithdrawParams({
+                    token: address(repayParams.market.data().underlyingCollateralToken),
+                    amount: repayParams.collateralWithdrawAmount,
+                    to: address(this)
+                })
+            );
+
+            // Swap collateral for borrow token
+            _swapCollateral(
+                address(repayParams.market.data().underlyingCollateralToken),
+                address(repayParams.market.data().underlyingBorrowToken),
+                repayParams.swapParams
+            );
+
+            // Approve and repay flash loan
+            IERC20Metadata(assets[0]).forceApprove(address(POOL), totalDebt);
+        }
 
         return true;
     }
